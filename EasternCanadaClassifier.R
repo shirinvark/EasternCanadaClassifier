fbd41f56759e101c8e45e86b7c4875455b0b2978
Shirin Varkouhi <145788851+shirinvark@users.noreply.github.com>
2026-02-26 21:41:55 -0500

    Initial module structure

---
+++ EasternCanadaClassifier.R
@@ -0,0 +1,122 @@
+## Everything in this file and any files in the R directory are sourced during `simInit()`;
+## all functions and objects are put into the `simList`.
+## To use objects, use `sim$xxx` (they are globally available to all modules).
+## Functions can be used inside any function that was sourced in this module;
+## they are namespaced to the module, just like functions in R packages.
+## If exact location is required, functions will be: `sim$.mods$<moduleName>$FunctionName`.
+defineModule(sim, list(
+  name = "EasternCanadaClassifier",
+  description = "Classifies harvestable landbase into analysis units and computes area summaries.",
+  keywords = c("analysis units", "classification", "harvestable landbase", "AAC")
+  ,
+  authors = structure(list(list(given = c("Shirin"), family = "Varkouhi", role = c("aut", "cre"), email = "shirin.varkuhi@gmail.com", comment = NULL)), class = "person"),
+  childModules = character(0),
+  version = list(EasternCanadaClassifier = "0.0.0.9000"),
+  timeframe = as.POSIXlt(c(NA, NA)),
+  timeunit = "year",
+  citation = list("citation.bib"),
+  documentation = list("NEWS.md", "README.md", "EasternCanadaClassifier.Rmd"),
+  reqdPkgs = list("SpaDES.core (>= 3.0.4)", "terra"),
+  parameters = bindrows(
+    #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
+    defineParameter(".plots", "character", "screen", NA, NA,
+                    "Used by Plots function, which can be optionally used here"),
+    defineParameter(".plotInitialTime", "numeric", start(sim), NA, NA,
+                    "Describes the simulation time at which the first plot event should occur."),
+    defineParameter(".plotInterval", "numeric", NA, NA, NA,
+                    "Describes the simulation time interval between plot events."),
+    defineParameter(".saveInitialTime", "numeric", NA, NA, NA,
+                    "Describes the simulation time at which the first save event should occur."),
+    defineParameter(".saveInterval", "numeric", NA, NA, NA,
+                    "This describes the simulation time interval between save events."),
+    defineParameter(".studyAreaName", "character", NA, NA, NA,
+                    "Human-readable name for the study area used - e.g., a hash of the study",
+                          "area obtained using `reproducible::studyAreaName()`"),
+    ## .seed is optional: `list('init' = 123)` will `set.seed(123)` for the `init` event only.
+    defineParameter(".seed", "list", list(), NA, NA,
+                    "Named list of seeds to use for each event (names)."),
+    defineParameter(".useCache", "logical", FALSE, NA, NA,
+                    "Should caching of events or module be used?")
+  ),
+    #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
+    inputObjects = bindrows(
+      expectsInput(
+        "Landbase",
+        objectClass = "list",
+        desc = "Harvestable landbase from EasternCanadaLandbase",
+        sourceURL = NA
+      )
+    ),
+  ),
+    #createsOutput("objectName", "objectClass", "output object description", ...),
+  outputObjects = bindrows(
+    createsOutput(
+      "analysisUnitMap",
+      "SpatRaster",
+      "Raster assigning harvestable pixels to analysis units."
+    ),
+    createsOutput(
+      "AU_summaries",
+      "data.frame",
+      "Area summaries per analysis unit."
+    ),
+    createsOutput(
+      "ageAreaTable",
+      "data.frame",
+      "Harvestable area by analysis unit and age class."
+    ),
+    createsOutput(
+      "totalEligibleArea_ha",
+      "numeric",
+      "Total eligible area (ha)."
+    ),
+    createsOutput(
+      "totalHarvestableArea_ha",
+      "numeric",
+      "Total harvestable area (ha)."
+    )
+  ))
+doEvent.EasternCanadaClassifier <- function(sim, eventTime, eventType) {
+  switch(
+    eventType,
+    init = {
+      sim <- Init(sim)
+    },
+    warning(noEventWarning(sim))
+  )
+  invisible(sim)
+}
+
+
+
+
+.inputObjects <- function(sim) {
+  # Any code written here will be run during the simInit for the purpose of creating
+  # any objects required by this module and identified in the inputObjects element of defineModule.
+  # This is useful if there is something required before simulation to produce the module
+  # object dependencies, including such things as downloading default datasets, e.g.,
+  # downloadData("LCC2005", modulePath(sim)).
+  # Nothing should be created here that does not create a named object in inputObjects.
+  # Any other initiation procedures should be put in "init" eventType of the doEvent function.
+  # Note: the module developer can check if an object is 'suppliedElsewhere' to
+  # selectively skip unnecessary steps because the user has provided those inputObjects in the
+  # simInit call, or another module will supply or has supplied it. e.g.,
+  # if (!suppliedElsewhere('defaultColor', sim)) {
+  #   sim$map <- Cache(prepInputs, extractURL('map')) # download, extract, load file from url in sourceURL
+  # }
+
+  #cacheTags <- c(currentModule(sim), "function:.inputObjects") ## uncomment this if Cache is being used
+  dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
+  message(currentModule(sim), ": using dataPath '", dPath, "'.")
+
+  # ! ----- EDIT BELOW ----- ! #
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+ggplotFn <- function(data, ...) {
+  ggplot2::ggplot(data, ggplot2::aes(TheSample)) +
+    ggplot2::geom_histogram(...)
+}
+
---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# EasternCanadaClassifier 0.0.1 (26 February 2026)
+
+- initial module version
