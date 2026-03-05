dc5380658261024dcb4e4020274f59ee9541ca3f
Shirin Varkouhi <145788851+shirinvark@users.noreply.github.com>
2026-03-05 15:39:40 -0500

    init

--- R/init.R
+++ R/init.R
@@ -1,84 +1,86 @@
 Init <- function(sim) {
   
-  planningRaster <- sim$Landbase$baseData$planningRaster
-  landcover      <- sim$Landbase$baseData$landcover
-  standAge       <- sim$Landbase$baseData$standAge
-  harvestable    <- sim$Landbase$fractional$harvestableFraction
+  message("Building analysisUnitMap from LandR state")
   
-  message("Building simple analysisUnitMap")
+  dt <- data.table::as.data.table(sim$cohortData)
   
-  analysisUnitMap <- terra::ifel(
-    harvestable > 0,
-    terra::classify(
-      landcover,
-      rcl = matrix(
-        c(210, 1,
-          220, 2,
-          230, 3),
-        ncol = 2,
-        byrow = TRUE
-      )
-    ),
-    NA
+  # ----------------------------
+  # species grouping
+  # ----------------------------
+  
+  conifer <- c(
+    "Abie_bal",
+    "Pice_mar",
+    "Pinu_ban",
+    "Pinu_res",
+    "Pinu_str"
   )
+  
+  dt[, type := ifelse(
+    speciesCode %in% conifer,
+    "conifer",
+    "broadleaf"
+  )]
+  
+  # ----------------------------
+  # biomass per pixelGroup
+  # ----------------------------
+  
+  summaryTable <- dt[, .(
+    volume = sum(B)
+  ), by = .(pixelGroup, age, type)]
+  
+  summaryWide <- data.table::dcast(
+    summaryTable,
+    pixelGroup + age ~ type,
+    value.var = "volume",
+    fill = 0
+  )
+  
+  # ----------------------------
+  # proportions
+  # ----------------------------
+  
+  summaryWide[, total := conifer + broadleaf]
+  
+  summaryWide[, prop_conifer := conifer / total]
+  
+  summaryWide[, prop_broadleaf := broadleaf / total]
+  
+  # ----------------------------
+  # simple classifier
+  # ----------------------------
+  
+  summaryWide[, AU := "Mixed"]
+  
+  summaryWide[prop_conifer >= 0.7, AU := "Conifer"]
+  
+  summaryWide[prop_broadleaf >= 0.7, AU := "Broadleaf"]
+  
+  summaryWide[, AU_id := as.numeric(as.factor(AU))]
+  
+  # ----------------------------
+  # lookup table
+  # ----------------------------
+  
+  lookup <- summaryWide[, .(pixelGroup, AU_id)]
+  
+  # ----------------------------
+  # build raster
+  # ----------------------------
+  
+  analysisUnitMap <- sim$pixelGroupMap
+  
+  terra::values(analysisUnitMap) <- lookup$AU_id[
+    match(
+      terra::values(sim$pixelGroupMap),
+      lookup$pixelGroup
+    )
+  ]
   
   sim$analysisUnitMap <- analysisUnitMap
   
-  # --- cell area ---
-  resXY <- terra::res(planningRaster)
-  cellArea_ha <- (resXY[1] * resXY[2]) / 10000
-  
-  # --- Eligible mask ---
-  isHarvestEligible <- harvestable > 0
-  
-  # --- Zonal summaries ---
-  eligibleArea_by_AU <- terra::zonal(
-    isHarvestEligible,
-    analysisUnitMap,
-    fun = "sum",
-    na.rm = TRUE
-  )
-  
-  harvestableArea_by_AU <- terra::zonal(
-    harvestable,
-    analysisUnitMap,
-    fun = "sum",
-    na.rm = TRUE
-  )
-  
-  # اگر zonal خالی بود
-  if (is.null(eligibleArea_by_AU) || nrow(eligibleArea_by_AU) == 0) {
-    sim$AU_summaries <- data.frame()
-    
-  } else {
-    
-    colnames(eligibleArea_by_AU) <- c("zone", "eligibleCells")
-    colnames(harvestableArea_by_AU) <- c("zone", "harvestableFraction")
-    
-    AU_summaries <- merge(
-      eligibleArea_by_AU,
-      harvestableArea_by_AU,
-      by = "zone",
-      all = TRUE
-    )
-    
-    AU_summaries$eligibleArea_ha <-
-      AU_summaries$eligibleCells * cellArea_ha
-    
-    AU_summaries$harvestableArea_ha <-
-      AU_summaries$harvestableFraction * cellArea_ha
-    
-    sim$AU_summaries <- AU_summaries
-  }
-  
-  # --- Totals ---
-  sim$totalEligibleArea_ha <-
-    terra::global(isHarvestEligible, "sum", na.rm = TRUE)[1,1] * cellArea_ha
-  
-  sim$totalHarvestableArea_ha <-
-    terra::global(harvestable, "sum", na.rm = TRUE)[1,1] * cellArea_ha
-  
-  sim$ageAreaTable <- data.frame()
+  message("analysisUnitMap created")
   
   return(sim)
 }
