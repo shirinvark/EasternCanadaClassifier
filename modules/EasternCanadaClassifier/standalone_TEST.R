822313fb03873d1aa1e28e82c2e4e18b93de5f61
Shirin Varkouhi <145788851+shirinvark@users.noreply.github.com>
2026-03-06 07:52:26 -0500

    Working version of EasternCanadaClassifier classifier\

---
+++ modules/EasternCanadaClassifier/standalone_TEST.R
@@ -0,0 +1,107 @@
+rm(list = ls())
+gc()
+
+library(SpaDES.core)
+library(SpaDES.project)
+library(terra)
+library(sf)
+library(data.table)
+
+## =========================================================
+## 1) SET PATHS
+## =========================================================
+
+setPaths(
+  cachePath   = "E:/EasternCanadaClassifier/cache",
+  inputPath   = "E:/EasternCanadaClassifier/inputs",
+  outputPath  = "E:/EasternCanadaClassifier/outputs",
+  modulePath  = "E:/EasternCanadaClassifier/modules",
+  scratchPath = "E:/EasternCanadaClassifier/scratch"
+)
+
+print(getPaths())
+
+## =========================================================
+## 2) DOWNLOAD MODULE
+## =========================================================
+
+SpaDES.project::getModule(
+  modules    = "shirinvark/EasternCanadaClassifier",
+  modulePath = getPaths()$modulePath,
+  overwrite  = TRUE
+)
+
+## =========================================================
+## 3) CREATE TEST DATA FOR NEW CLASSIFIER
+## =========================================================
+
+# pixelGroup raster
+
+pixelGroupMap <- rast(nrows=10, ncols=10, xmin=0, xmax=1000, ymin=0, ymax=1000)
+values(pixelGroupMap) <- sample(1:20, 100, replace=TRUE)
+
+# cohortData (LandR style)
+
+cohortData <- data.table(
+  pixelGroup = sample(1:20, 200, replace = TRUE),
+  speciesCode = sample(
+    c("Abie_bal","Pice_mar","Pinu_ban","Pinu_res","Pinu_str","Acer_sah"),
+    200,
+    replace = TRUE
+  ),
+  age = sample(1:120, 200, replace = TRUE),
+  B = runif(200, 1, 50)
+)
+
+## =========================================================
+## 4) INIT SIMULATION
+## =========================================================
+
+sim <- simInit(
+  times = list(start = 0, end = 1),
+  modules = "EasternCanadaClassifier",
+  objects = list(
+    cohortData = cohortData,
+    pixelGroupMap = pixelGroupMap
+  ),
+  options = list(
+    spades.checkpoint = FALSE,
+    spades.save       = FALSE,
+    spades.progress   = FALSE
+  )
+)
+
+## =========================================================
+## 5) RUN
+## ========================================================
+
+system.time({
+  sim <- spades(sim)
+})
+
+## =========================================================
+## 6) CHECK OUTPUT
+## =========================================================
+library(terra)
+
+cellArea <- prod(res(sim$analysisUnitMap)) / 10000
+
+areaTable <- as.data.frame(freq(sim$analysisUnitMap))
+
+areaTable$area_ha <- areaTable$count * cellArea
+
+areaTable
+cat("\nObjects in sim:\n")
+print(names(sim))
+
+if ("analysisUnitMap" %in% names(sim)) {
+  
+  plot(sim$analysisUnitMap, main = "Analysis Unit Map")
+  
+} else {
+  
+  stop("analysisUnitMap was not created")
+}
+
+cat("\nClassifier test completed successfully.\n")
+
