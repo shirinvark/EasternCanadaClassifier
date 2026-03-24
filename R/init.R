223bc01804cbb683c8b0eb85de4437186f7a7b58
Shirin Varkouhi <145788851+shirinvark@users.noreply.github.com>
2026-03-24 15:40:38 -0400

    Correctly run

--- R/init.R
+++ R/init.R
@@ -168,7 +168,7 @@ Init <- function(sim) {
     cohortDT[speciesCode %in% speciesGroups[[g]], group := g]
   }
   # 4️⃣ Remove species not assigned to any group (e.g. Acer)
-  cohortDT <- cohortDT[!is.na(group)]
+  cohortDT[, group := "Unknown"]
   cat("Group counts:\n")
   print(table(cohortDT$group))
   cat("\n")
