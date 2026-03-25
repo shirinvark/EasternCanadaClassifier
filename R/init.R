27e5eb2fa611d84420bacc178e84830cd8028c17
Shirin Varkouhi <145788851+shirinvark@users.noreply.github.com>
2026-03-25 17:41:37 -0400

    cohort

--- R/init.R
+++ R/init.R
@@ -222,11 +222,8 @@ Init <- function(sim) {
   # =========================================================
   
   cohortDT <- data.table::copy(cohortData)
-  setnames(
-    cohortDT,
-    "pixelGroup.NFI_MODIS250m_2001_kNN_Structure_Biomass_TotalLiveAboveGround_v1",
-    "pixelGroup"
-  )
+  
+  print(names(cohortDT))  # 👈 اینو اضافه کن
   # 2️⃣ Create group column
   cohortDT[, group := NA_character_]
   for (g in names(speciesGroups)) {
