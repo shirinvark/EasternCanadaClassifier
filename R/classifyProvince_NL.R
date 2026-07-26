d90830880c5894b4e2e52e7df50d24f2463f02fc
Shirin Varkouhi <145788851+shirinvark@users.noreply.github.com>
2026-07-25 22:58:33 -0400

    age and Biomass

--- R/classifyProvince_NL.R
+++ R/classifyProvince_NL.R
@@ -87,64 +87,7 @@ classifyProvince_NL <- function(sim) {
     }
   }
   
-  # ===================================================
-  # STAGE 1: READ RAW YIELD CURVES
-  # ===================================================
-  #
-  # Goal:
-  #   Build the initial yield curve library for Newfoundland.
-  #
-  # At the end of this stage we should have all raw yield
-  # table files and species mapping files available locally.
-  #
-  # Inputs:
-  #   - Newfoundland yield table files (*.yld)
-  #   - speciesGroups.txt
-  #   - mapSpeciesGroups.txt
-  #
-  # Yield tables at this stage are still in their original
-  # species-code format (e.g. WSv, BSv, BFv, TLv, etc.).
-  #
-  # No species grouping, standardization, classification,
-  # distance calculations, or curve matching have occurred yet.
-  #
-  # Conceptually the output of this stage is:
-  #
-  #   Raw Yield Table Library
-  #
-  #        Region / Yield Table
-  #                 ↓
-  #            Raw Curves
-  #                 ↓
-  #            Age Classes
-  #                 ↓
-  #          Species Volumes
-  #
-  # Similar to Ontario, this stage is responsible only for:
-  #
-  #   ✓ downloading files
-  #   ✓ reading raw yield tables
-  #   ✓ preparing a local yield table library
-  #
-  # This stage does NOT:
-  #
-  #   ✗ rewrite species groups
-  #   ✗ aggregate species
-  #   ✗ convert to proportions
-  #   ✗ assign regions to pixels
-  #   ✗ compare cohorts to curves
-  #   ✗ select best curves
-  #
-  # Those steps occur later in the classifier pipeline.
-  #
-  # Expected next stage:
-  #
-  #   Stage 2:
-  #   Rewrite raw species codes into standardized
-  #   Newfoundland species groups.
-  #
-  # ===================================================
-  #تا اینجا درست
+  
   
   source(
     file.path(
@@ -277,7 +220,6 @@ classifyProvince_NL <- function(sim) {
   
   yieldTables_NL <- list()
   cleanOriginalYieldTables_NL <- list()
-  # conversionFactor <- 1000 * 0.5 / 0.8
   
   for (i in seq_len(nrow(AU_table))) {
     
@@ -311,7 +253,6 @@ classifyProvince_NL <- function(sim) {
     ####################new
     
     headers <- parseYLDHeaders(lines)
-    print(headers)
     comm <- community
     reg  <- region
     header <- headers[
@@ -320,21 +261,7 @@ classifyProvince_NL <- function(sim) {
         density == "d2" &
         region == reg
     ]
-    cat("\n========================\n")
-    cat("AU:", au, "\n")
-    cat("Community:", comm, "\n")
-    cat("Region:", reg, "\n")
-    cat("Matched headers:", nrow(header), "\n")
     
-    if (nrow(header) != 1) {
-      print(
-        headers[
-          community == comm &
-            quality == "medium" &
-            density == "D2"
-        ]
-      )
-    }
     
     stopifnot(nrow(header) == 1)
     speciesInfo <- parseSpecies(
@@ -373,7 +300,6 @@ classifyProvince_NL <- function(sim) {
         difference = raw_total - group_total
       )
       
-      print(tmp_check)
       
       cat(
         "\nMAX DIFFERENCE:",
@@ -388,64 +314,44 @@ classifyProvince_NL <- function(sim) {
       length.out = length(curve_data[[1]])
     )
     
+    maxAge <- max(ages)
+    
     dt_curve <- data.table(
-      AC10 = ages
+      AC10 = 1:maxAge
     )
     
     for (sp in names(curve_data)) {
-      dt_curve[[sp]] <- curve_data[[sp]]
-    }
-    # # Steve:
-    # # Convert yield volume (m3/ha) to approximate biomass (kg/ha)
-    # 
-    # dt_curve[
-    #   ,
-    #   (names(curve_data)) := lapply(
-    #     .SD,
-    #     function(x) x * conversionFactor
-    #   ),
-    #   .SDcols = names(curve_data)
-    # ]
-    
-    if (au == "Central_BF") {
       
-      cat("\n==============================\n")
-      cat("RAW dt_curve BEFORE STANDARDIZATION\n")
-      cat("==============================\n")
-      
-      print(dt_curve)
-      
-    }
-    if (au == "Aphid_bF") {
-      
-      cat("\n==============================\n")
-      cat("Aphid_bF BEFORE CLASSIFIER\n")
-      cat("==============================\n")
-      
-      print(
-        dt_curve[
-          AC10 %in% c(0,10,20,30,40,50,60,70,80,90),
-          .(
-            AC10,
-            blackSpruce_NL,
-            balsamFir_NL,
-            tamarack_NL,
-            otherConifer_NL,
-            broadleaf_NL
-          )
-        ]
+      tmp <- standardizeYieldCurve(
+        ages    = ages,
+        volumes = curve_data[[sp]],
+        maxAge  = maxAge
       )
+      
+      dt_curve[[sp]] <- tmp$volume
     }
+    
+    # -------------------------------------------------------
+    # Convert yield volume (m3/ha) to approximate biomass (kg/ha)
+    # Steve's suggested conversion
+    # -------------------------------------------------------
+    summary(dt_curve)
+    conversionFactor <- 1000 * 0.5 / 0.8
+    
+    species_cols <- setdiff(names(dt_curve), "AC10")
+    
+    dt_curve[
+      ,
+      (species_cols) := lapply(
+        .SD,
+        function(x) x * conversionFactor
+      ),
+      .SDcols = species_cols
+    ]
     
     
     yieldTables_NL[[au]] <- dt_curve
-    
   }
-  # =========================================================
-  # BUILD pixel_region FROM NL_YCF
-  # =========================================================
-  ####فعلا بمونه توی مدل چون هنوز پیکسل گروپ نداریم
-  
   # =========================================================
   # BUILD pixel_region FROM NL_YCF
   # =========================================================
@@ -487,11 +393,7 @@ classifyProvince_NL <- function(sim) {
     touches = TRUE
   )
   
-  print(region_raster)
   
-  print(terra::cats(region_raster))
-  
-  print(levels(region_raster))
   
   
   if (!terra::same.crs(
@@ -557,7 +459,6 @@ classifyProvince_NL <- function(sim) {
   
   lut <- levels(region_raster)[[1]]
   
-  print(lut)
   
   
   pixel_region[
@@ -622,21 +523,8 @@ classifyProvince_NL <- function(sim) {
     value.var = "B",
     fill = 0
   )
-  cat("\n===== AGE DISTRIBUTION =====\n")
   
-  print(
-    cohort_wide[
-      ,
-      .N,
-      by = age
-    ][order(age)]
-  )
-  print(sort(unique(cohort_wide$age))[1:30])
-  cohort_wide[
-    ,
-    .N,
-    by = age
-  ][order(age)]
+  
   group_cols <- setdiff(
     names(cohort_wide),
     c("pixelGroup", "age", "total")
@@ -647,27 +535,12 @@ classifyProvince_NL <- function(sim) {
     total := rowSums(.SD),
     .SDcols = group_cols
   ]
-  cat("\n===== AGE DISTRIBUTION =====\n")
   
-  print(
-    cohort_wide[
-      ,
-      .N,
-      by = age
-    ][order(age)]
-  )
-  print("===== COHORT WIDE CHECK =====")
-  print(head(cohort_wide))
-  
-  print(summary(cohort_wide$total))
-  
-  print(any(is.na(cohort_wide$total)))
   
   cohort_wide <- cohort_wide[
     total > 0
   ]
   
-  print(names(cohort_wide))
   ####normalization
   group_cols <- setdiff(
     names(cohort_wide),
@@ -685,89 +558,7 @@ classifyProvince_NL <- function(sim) {
     stop("❌ No matching species groups between cohort and yield")
   }
   
-  #cohort_wide[
-  #,
-  # (prop_cols) := lapply(
-  # .SD,
-  #function(x) x / total
-  #  ),
-  # .SDcols = prop_cols
-  #]
   
-  ####pixel by region
-  # shp2 <- terra::project(
-  # shp,
-  # terra::crs(sim$pixelGroupMap)
-  #)
-  #print(region_raster)
-  
-  # print(
-  #terra::freq(
-  # region_raster,
-  # digits = 0
-  # )
-  # )
-  # summary(
-  #  terra::values(region_raster)
-  #)
-  #region_raster <- terra::rasterize(
-  #shp2,
-  #sim$pixelGroupMap,
-  # field = "YCF"
-  #)
-  
-  #pg <- data.table::as.data.table(
-  #terra::values(sim$pixelGroupMap)
-  #)
-  
-  #  reg <- data.table::as.data.table(
-  #   terra::values(region_raster)
-  #)
-  
-  #setnames(pg, names(pg), "pixelGroup")
-  #setnames(reg, names(reg), "region")
-  
-  #pixel_region <- cbind(pg, reg)
-  #cat("\n===== BEFORE FILTER =====\n")
-  
-  #print(head(pixel_region, 20))
-  
-  #print(dim(pixel_region))
-  #pixel_region <- pixel_region[
-  #!is.na(pixelGroup) &
-  #   !is.na(region)
-  #]
-  
-  #pixel_region[
-  #,
-  #region := gsub(
-  #"^NL_",
-  # "",
-  #  as.character(region)
-  # )
-  #]
-  
-  
-  
-  #lut <- levels(region_raster)[[1]]
-  
-  # pixel_region[
-  #  ,
-  # region := lut$YCF[
-  #  match(region, lut$ID)
-  #]
-  #]
-  
-  #pixel_region[
-  # ,
-  #region := gsub(
-  # "^NL_",
-  #"",
-  #region
-  #)
-  #]
-  
-  ##yield by region
   
   yield_by_region <- list()
   
@@ -777,12 +568,7 @@ classifyProvince_NL <- function(sim) {
       region == reg,
       AU
     ]
-    cat("\n===== yieldTables_NL =====\n")
-    print(length(yieldTables_NL))
-    print(head(names(yieldTables_NL), 10))
     
-    cat("\n===== region_aus =====\n")
-    print(region_aus)
     region_list <- lapply(
       region_aus,
       function(au) {
@@ -790,8 +576,7 @@ classifyProvince_NL <- function(sim) {
         dt <- copy(
           yieldTables_NL[[au]]
         )
-        print(class(yieldTables_NL[[au]]))
-        print(is.data.table(yieldTables_NL[[au]]))
+        
         dt[, AU := au]
         
         dt[, zone := reg]
@@ -805,52 +590,9 @@ classifyProvince_NL <- function(sim) {
       fill = TRUE
     )
     
-    if (reg == "Aphid") {
-      
-      cat("\n===== APHID TOTAL BY AGE =====\n")
-      
-      print(
-        yield_by_region[["Aphid"]][
-          AC10 %in% c(10,20,30,40,50,60,70),
-          .(
-            total =
-              blackSpruce_NL +
-              balsamFir_NL +
-              tamarack_NL +
-              otherConifer_NL +
-              broadleaf_NL
-          ),
-          by = .(AU, AC10)
-        ]
-      )
-      
-    }   ## ← این آکولاد خیلی مهم است
     
   }   ## ← این هم پایان حلقه for
-  cat("\n===== BAREAST TOTAL BY AGE =====\n")
   
-  print(
-    yield_by_region[["BarEast"]][
-      AC10 %in% c(10,20,30,40,50,60,70,80),
-      .(
-        total =
-          blackSpruce_NL +
-          balsamFir_NL +
-          tamarack_NL +
-          otherConifer_NL +
-          broadleaf_NL
-      ),
-      by = .(AU, AC10)
-    ]
-  )
-  cat("\n===== yield_by_region names =====\n")
-  print(names(yield_by_region))
-  
-  cat("\n===== First region =====\n")
-  print(names(yield_by_region)[1])
-  
-  cat("\n===== Columns =====\n")
-  print(names(yield_by_region[[1]]))
   # =========================================================
   # NORMALIZE CURVES ONCE
   # =========================================================
@@ -922,40 +664,8 @@ classifyProvince_NL <- function(sim) {
   # ======================================================
   # CLASSIFY ONE PIXEL
   # ====================================================
-  cat("\n===== curve_cols =====\n")
-  print(curve_cols)
-  
-  cat("\n===== cohort_wide =====\n")
-  print(names(cohort_wide))
-  
-  cat("\n===== yield_by_region =====\n")
-  print(names(yield_by_region[[1]]))
-  cat("\n===== AGE 10 CURVES =====\n")
-  
-  print(
-    yield_by_region[[1]][
-      AC10 == 10,
-      .(
-        AU,
-        blackSpruce_NL,
-        balsamFir_NL,
-        tamarack_NL,
-        otherConifer_NL,
-        broadleaf_NL
-      )
-    ]
-  )
-  cat("\n===== curve_cols =====\n")
-  print(curve_cols)
-  
-  cat("\n===== cohort columns =====\n")
-  print(names(cohort_wide))
-  
-  cat("\n===== yield columns =====\n")
-  print(names(yield_by_region[[1]]))
   results <- cohort_classifiable[, {
-    if (.GRP %% 10000 == 0)
-      cat("Processed:", .GRP, "\n")
+    
     
     cohort_vec <- unlist(
       .SD[1, curve_cols, with = FALSE],
@@ -1004,73 +714,13 @@ classifyProvince_NL <- function(sim) {
       
       ratio <- pixel_total / curve_total
       ################
-      if (!exists(".debugDone", inherits = FALSE)) {
-        
-        assign(".debugDone", TRUE, envir = .GlobalEnv)
-        
-        cat("\n=============================\n")
-        cat("DEBUG FIRST PIXEL\n")
-        cat("=============================\n")
-        
-        cat("PixelGroup:", .BY$pixelGroup, "\n")
-        cat("Region:", region, "\n")
-        cat("Age:", age_val, "\n")
-        
-        cat("\nPixel biomass by group:\n")
-        print(cohort_vec)
-        
-        cat("\nPixel total biomass:\n")
-        print(pixel_total)
-        
-        cat("\nCurve totals:\n")
-        print(summary(curve_total))
-        
-        cat("\nRatio:\n")
-        print(summary(ratio))
-        
-        cat("\nCandidate curves:\n")
-        print(nrow(curves))
-        
-        cat("\nSelected AC10:\n")
-        print(unique(curves$AC10))
-        
-        cat("\nCandidate AUs:\n")
-        print(unique(curves$AU))
-      }
+      
       ##############
       curves_filtered <- curves[
         ratio >= 0.6 &
           ratio <= (1 / 0.6)
       ]
-      if (age_val > 0 && !exists(".printedDebug", inherits = FALSE)) {
-        
-        .printedDebug <- TRUE
-        
-        cat("\n==============================\n")
-        cat("FIRST NONZERO AGE PIXEL\n")
-        cat("==============================\n")
-        
-        cat("\nPixel age:\n")
-        print(age_val)
-        
-        cat("\nSelected curve ages:\n")
-        print(unique(curves$AC10))
-        
-        cat("\nPixel total:\n")
-        print(pixel_total)
-        
-        cat("\nCurve totals:\n")
-        print(summary(curve_total))
-        
-        cat("\nRatio:\n")
-        print(summary(ratio))
-        
-        cat("\nBefore filter:\n")
-        print(nrow(curves))
-        
-        cat("\nAfter filter:\n")
-        print(nrow(curves_filtered))
-      }
+      
       if (nrow(curves_filtered) > 0) {
         curves <- curves_filtered
       }
@@ -1090,6 +740,8 @@ classifyProvince_NL <- function(sim) {
           (curves_mat - cohort_mat)^2
         )
       )
+      
+      
       
       best_idx <- which.min(dists)
       
